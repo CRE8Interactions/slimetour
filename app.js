3e2ca6162b9f7694da83a867825fae57d59b112d
Chaz Haskins <chazhaskins@Chazs-MacBook-Pro.local>
2022-01-03 13:18:11 -0500

    Test

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
+const host = 'localhost'
 const port = process.env.PORT || 8080
 const fs = require('fs')
 const https = require('https')
+const http = require('http')
 
 const options = {
   key: fs.readFileSync('certs/slime.key'),
@@ -13,6 +15,16 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-https.createServer(options, app).listen(port, () => {
-  console.log(`App running on port: ${port}`)
+const requestListener = function (req, res) {
+  res.writeHead(200);
+  res.end("Coming Soon");
+};
+
+const server = http.createServer(requestListener);
+server.listen(port, host, () => {
+    console.log(`Server is running on http://${host}:${port}`);
 });
+
+// https.createServer(options, app).listen(port, () => {
+//   console.log(`App running on port: ${port}`)
+// });
