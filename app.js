0c9310ee1f1e5ed14e28bd0406f2f3507f839f33
Chaz Haskins <chazhaskins@Chazs-MacBook-Pro.local>
2022-01-03 12:29:11 -0500

    https

--- app.js
+++ app.js
@@ -1,21 +1,18 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 3000
-const fs = require('fs');
+const port = process.env.PORT || 8080
+const fs = require('fs')
+const https = require('https')
 
 const options = {
   key: fs.readFileSync('certs/slime.key'),
   cert: fs.readFileSync('certs/slimelifeshawtytour_com.chained.crt')
-};
+}
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-https.createServer(options, (req, res) => {
-  res.send('hello world\n');
-}).listen(port);
-
-app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`)
-})
+https.createServer(options, app).listen(port, () => {
+  console.log(`App running on port: ${port}`)
+});
