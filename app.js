6a1c1a4eac32dfa8448296960d5bdc19b6483105
Chaz Haskins <chazhaskins@Chazs-MacBook-Pro.local>
2022-01-03 12:10:53 -0500

    Initial Commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const { env } = require('@tensorflow/tfjs-core');
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+
+const options = {
+  key: fs.readFileSync('certs/slime.key'),
+  cert: fs.readFileSync('certs/slimelifeshawtytour_com.chained.crt')
+};
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+https.createServer(options, (req, res) => {
+  res.send('hello world\n');
+}).listen(port);
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`)
+})
